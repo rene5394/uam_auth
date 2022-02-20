d7b6f31d183b0d9b971708a805b1758b3907dbd4
Rene Torres <rene_edgardo_2@hotmail.com>
2022-02-19 19:43:48 -0600

    Delete the use of cookies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { dotenv } from '../config/env'
 import express, { Application, json } from 'express'
-import cookieParser from 'cookie-parser'
 import { loadControllers } from 'awilix-express'
 import loadContainer from './container'
 import cors from 'cors'
@@ -10,8 +9,6 @@ dotenv.config()
 const app: Application = express()
 
 app.use(json())
-
-app.use(cookieParser())
 
 app.use(cors())
 
